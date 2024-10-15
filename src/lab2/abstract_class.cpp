e75a90bdbf56df06fd2eb8fc9fdd3d79893a86ca
eestelle <yosiky@list.ru>
2024-10-15 13:53:41 +0300

    src/lab2/abstract_class.cpp: Add new information
    Signed-off-by: eestelle <yosiky@list.ru>

--- src/lab2/abstract_class.cpp
+++ src/lab2/abstract_class.cpp
@@ -5,23 +5,28 @@ public:
     virtual void printInfo(void) const = 0; /* You must to mark a function "<func> = 0" */
 };
 
-class Circle : Figure {
-public:
+/*
+ * If you don't say a type of inheritance, then the type will be used default:
+ * - public for structs
+ * - private for classes
+ */
+struct Circle : Figure {
     void printInfo(void) const override {
         std::cout << "It's Circle" << std::endl;
     }
 };
 
-class Square : Figure {
-public:
-    
+struct Square : Figure {
+
     /* If function will be uncommented we don't get error in main() */
-    // void printInfo(void) const override {
-    //     std::cout << "It's Square" << std::endl;
-    // }
+    void printInfo(void) const override {
+        std::cout << "It's Square" << std::endl;
+    }
 };
 
 int main() {
     Circle circle;
     Square square; /* We get error because printInfo() isn't overrided in Square */
+    circle.printInfo();
+    square.printInfo();
 }
